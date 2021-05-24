83f89e37f072b02123c10dab8acfb745fe01ad38
SGBotic <developer@sgbotic.com>
2021-05-24 12:35:39 +0800

    Update ubit-connect.ts

--- ubit-connect.ts
+++ ubit-connect.ts
@@ -97,7 +97,7 @@ namespace SGBotic {
     //% weight=85 blockExternalInputs=true blockGap=8
     //% blockId=oled96_set_text
     //% row.min=0 row.max=3
-     //% column.min=0 column.max=15
+     //% column.min=0 column.max=125
     //% block="move cursor to row %row| column %column"
     export function setTextXY(row: number, column: number) {
         
